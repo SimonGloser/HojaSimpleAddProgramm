d5d059924649eb02bf2460b38764adf2cb46a394
SimonGloser <simon.gloser@googlemail.com>
2019-04-27 12:44:40 +0200

    add multiplication

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	    System.out.println("10 + 2883 + 444 = " + (10 + 2883 + 444));
+        System.out.println("10 * 2883 * 444 = " + (10 * 2883 * 444));
     }
 }
